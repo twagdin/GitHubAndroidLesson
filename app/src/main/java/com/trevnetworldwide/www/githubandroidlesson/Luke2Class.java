80d3052a081c6f9ae5f578cdc21ce9553286d7d8
Khori Armstrong <khoriarmstrong@gmail.com>
2015-03-04 11:46:35 -0500

    I wrecked your file, Luke!

--- app/src/main/java/com/trevnetworldwide/www/githubandroidlesson/Luke2Class.java
+++ app/src/main/java/com/trevnetworldwide/www/githubandroidlesson/Luke2Class.java
@@ -4,4 +4,13 @@ package com.trevnetworldwide.www.githubandroidlesson;
  * Created by yangjiachang on 15-03-04.
  */
 public class Luke2Class {
+
+    int numbercount = 59348;
+
+
+    public Luke2Class() {
+        NewClassGuys ncg = new NewClassGuys();
+        // NEW~!!!
+    }
+
 }
