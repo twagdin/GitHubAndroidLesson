a97ba319cceb6d4a7a80fca4494a72e0a2ffad5c
twagdin <twagdin@gmail.com>
2015-03-04 11:54:08 -0500

    Trevors Class Created

---
+++ app/src/main/java/com/trevnetworldwide/www/githubandroidlesson/TrevorsClass.java
@@ -0,0 +1,10 @@
+package com.trevnetworldwide.www.githubandroidlesson;
+
+/**
+ * Created by Trevor on 2015-03-04.
+ */
+public class TrevorsClass {
+    public void hello(){
+        //Hello
+    }
+}
