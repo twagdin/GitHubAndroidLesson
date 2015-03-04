c01a980718fe7a671e6f3b108227a891666d900f
Khori Armstrong <khoriarmstrong@gmail.com>
2015-03-04 11:41:16 -0500

    New class, guys!

---
+++ app/src/main/java/com/trevnetworldwide/www/githubandroidlesson/NewClassGuys.java
@@ -0,0 +1,10 @@
+package com.trevnetworldwide.www.githubandroidlesson;
+
+/**
+ * Created by Owner on 04/03/2015.
+ */
+public class NewClassGuys {
+    public NewClassGuys() {
+        // WHOA!!
+    }
+}
